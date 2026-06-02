d010f00c04518e50fd3552fd8e7b146209d49371
stitch8626 <userelement668@gmail.com>
2026-06-03 02:44:35 +0300

    -

--- pythone/metanit/task if3.py
+++ pythone/metanit/task if3.py
@@ -12,39 +12,30 @@
 
 summary = int(input("Введите сумму: "))
 
-if summary > 25000:
+if summary < 5000:
+    discount = 5
+    discount_amount = (summary * discount) / 100
+    final_summary = summary - discount_amount
+    print(f"Скидка: {discount_amount}")
+    print(f"Сумма с учетом скидки : {final_summary}")
+
+elif summary < 15000:
+    discount = 12
+    discount_amount = (summary * discount) / 100
+    final_summary = summary - discount_amount
+    print(f"Скидка: {discount_amount}")
+    print(f"Сумма с учетом скидки : {final_summary}")
+
+elif summary < 25000:
     discount = 30
     discount_amount = (summary * discount) / 100
     final_summary = summary - discount_amount
     print(f"Скидка: {discount_amount}")
     print(f"Сумма с учетом скидки : {final_summary}")
 
-if summary < 25000:
+elif summary > 25000:
     discount = 20
     discount_amount = (summary * discount) / 100
     final_summary = summary - discount_amount
     print(f"Скидка: {discount_amount}")
     print(f"Сумма с учетом скидки : {final_summary}")
-
-
-
-# if summary  > 25000:
-#     discount = 0.30
-#     final_summary = summary * (1 - discount)
-#     print(f"{final_summary} со скидкой в 30% ")
-
-# if summary  > 25000:
-#     discount = 0.20
-#     final_summary = summary * (1 - discount)
-#     print(f"{final_summary} со скидкой в 20% ")
-
-# if summary  > 15000:
-#     discount = 0.12
-#     final_summary = summary * (1 - discount)
-#     print(f"{final_summary} со скидкой в 12% ")
-
-# if  summary > 5000:
-#     discount = 0.05
-#     final_summary = summary * (1 - discount)
-#     print(f"{final_summary} со скидкой в 5% ")
-
