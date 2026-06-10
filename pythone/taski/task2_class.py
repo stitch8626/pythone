9ea4d1a9d1e1ea3acdbf1fb8383f43acda7f6e7a
stitch8626 <userelement668@gmail.com>
2026-06-10 16:08:16 +0300

    Сделал таск 2 по классам

--- pythone/taski/task2_class.py
+++ pythone/taski/task2_class.py
@@ -1,7 +1,34 @@
+# Упражнение 2
+# Создайте класс BankAccount, который представляет банковский счет. 
+# Определите в этом классе атрибуты account_number и balance, которые представляют номер счета и баланс. 
+# Через параметры конструктора передайте этим атрибутам начальные значения.
+
+# Также в классе определите метод add, который принимает некоторую сумму и добавляет ее на баланс счета. 
+# И определите метод withdraw, который принимает некоторую сумму и снимает ее с баланса. При этом с баланса нельзя снять больше, чем имеется. 
+# Если на балансе недостаточно средств, то пользователю должно выводиться соответствующее сообщение.
+
 class BankAccount:
-    def bank_check(self, account_number=1, balance="1900"):
-        print(account_number, balance)
+    def __init__(self, balance= 0,account_number=1 ):
+        self.balance = balance
+        print(f"у акаунта {account_number} баланс: {balance}")
+
+    def add(self, summary_add=0):
+        self.summary_add = summary_add
+        print(f"к балансу {self.balance} прибавляем {summary_add} теперь баланс: ", end="")
+        self.balance = self.balance + summary_add
+        print(f"{self.balance}")
+
+    def withdraw(self, remove_balanse):
+        self.remove_balanse = remove_balanse
+        if remove_balanse > self.balance:
+            print("На балансе не достаточно средст")
+        else: 
+            print(f"С баланса {self.balance} списанны средства в виде {remove_balanse}")
+            self.balance = self.balance - remove_balanse
+            print(f"ваш баланс состовляет: {self.balance}")
 
 
-tom = BankAccount()
-tom.bank_check()
+
+tom = BankAccount(120)
+tom.add(1800)
+tom.withdraw(1200)
