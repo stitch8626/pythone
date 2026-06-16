6eb25c01075e7b77238a3c4e20bef059ff665af8
stitch8626 <userelement668@gmail.com>
2026-06-16 23:56:09 +0300

    обновнлениие, изучение классов

--- pythone/metanit/metanit2.0.py
+++ pythone/metanit/metanit2.0.py
@@ -2,12 +2,28 @@ class Person:
     def __init__(self, name, age):
         self.__name = name    # устанавливаем имя
         self.__age = age       # устанавливаем возраст
-                  
+ 
+    # сеттер для установки возраста
+    def set_age(self, age):
+        if 0 < age < 110:
+            self.__age = age
+        else:
+            print("Недопустимый возраст")
+ 
+    # геттер для получения возраста
+    def get_age(self):
+        return self.__age
+ 
+    # геттер для получения имени
+    def get_name(self):
+        return self.__name
+     
     def print_person(self):
-        print(f"Имя: {self.__name}\tВозраст: {self.__age}")
+        print(f"Имя: {self.__name}\nВозраст: {self.__age}")
           
   
 tom = Person("Tom", 39)
-tom.__name = "Человек-паук"     # пытаемся изменить атрибут __name
-tom.__age = -129                # пытаемся изменить атрибут __
-tom.print_person()       
+tom.print_person()  # Имя: Tom  Возраст: 39
+tom.set_age(-3486)  # Недопустимый возраст
+tom.set_age(25)
+tom.print_person()  # Имя: Tom  Возраст: 25
