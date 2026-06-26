b328bf2a6edac560ec510abed4b0e55f9de0dd51
stitch8626 <userelement668@gmail.com>
2026-06-26 03:48:43 +0300

    изучил наследование классов

--- pythone/metanit/metanit2.0.py
+++ pythone/metanit/metanit2.0.py
@@ -1,30 +1,14 @@
-class Person:
-    def __init__(self, name, age):
-        self.__name = name 
-        self.__age = age
-
-    
-    
-    
-    @property
-    def age(self):
-        return self.__age
-    
-    @age.setter
-    def age(self, age):
-        if 0 < age < 110:
-            self.__age = age
-        else:
-            print("Недопустимый возраст")
-
-    @property
-    def name(self):
-        return self.__name
-
-    def print_person(self):
-        print(f"Имя: {self.__name}\tВозраст: {self.__age}")
-
-
-
-tom = Person("Tom", 67)
-tom.print_person()
+class Employee:
+    def do(self):
+        print("Employee works")
+  
+class Student:
+    def do(self):
+        print("Student studies")
+  
+  
+# class WorkingStudent(Student,Employee):
+class WorkingStudent(Employee, Student):
+    pass
+ 
+print(WorkingStudent.mro())
