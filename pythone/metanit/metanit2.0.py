75591aad28eed83b75da20428fef10b54635c86c
stitch8626 <userelement668@gmail.com>
2026-06-28 05:02:53 +0300

    изучение Переопределение функционала

--- pythone/metanit/metanit2.0.py
+++ pythone/metanit/metanit2.0.py
@@ -1,14 +1,34 @@
-class Employee:
-    def do(self):
-        print("Employee works")
-  
-class Student:
-    def do(self):
-        print("Student studies")
-  
-  
-# class WorkingStudent(Student,Employee):
-class WorkingStudent(Employee, Student):
-    pass
- 
-print(WorkingStudent.mro())
+class Person: 
+    def __init__(self, name):
+        self.__name = name 
+
+
+    @property
+    def name(self):
+        return self.__name
+    
+    def display_info(self):
+        print(f"Имя {self.name}")
+    
+
+class Summary_bank(Person):
+
+    def __init__(self,name, summary):
+        super().__init__(name)
+        self.__summary = summary
+    
+
+    @property
+    def summary(self):
+        return self.__summary
+    
+
+    def dislpay_info(self):
+        super().display_info()
+        print(f"Сумма: {self.__summary}")
+
+
+
+
+tom = Summary_bank("Tom", 67)
+tom.dislpay_info()
