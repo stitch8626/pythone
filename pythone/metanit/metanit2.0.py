39dacaf5bf08611196d73161a9ab60fdb8b06c4a
stitch8626 <userelement668@gmail.com>
2026-07-17 01:13:31 +0300

    изучил isinstance

--- pythone/metanit/metanit2.0.py
+++ pythone/metanit/metanit2.0.py
@@ -1,30 +1,47 @@
-class Person:
- 
+class Person: 
     def __init__(self, name):
-        self.__name = name   # имя человека
- 
+        self.__name = name 
+
+
     @property
     def name(self):
         return self.__name
- 
+    
     def display_info(self):
-        print(f"Name: {self.__name}")
- 
- 
-class Employee(Person):
- 
-    def __init__(self, name, company):
-        super().__init__(name)
-        self.company = company
- 
-    def display_info(self):
-        super().display_info()
-        print(f"Company: {self.company}")
- 
-    def work(self):
-        print(f"{self.name} works")
- 
- 
-tom = Employee("Tom", "Microsoft")
-tom.display_info()  # Name: Tom
-                    # Company: Microsoft
+        print(f"Имя {self.name}")
+    
+
+class Summary_bank(Person):
+
+   def user_name(self):
+       print(f"клиент с именем {self.name}")
+
+
+class Student(Person):
+
+    def study(self):
+        print(f"имя студента {self.name}")
+
+
+
+def display_info(person):
+    if isinstance(person, Student):
+        person.study()
+    elif isinstance(person, Summary_bank):
+        person.user_name()
+    elif isinstance(person, Person):
+        person.display_info()
+
+
+yarik = Summary_bank("Yarik")
+
+tom = Student("Tom")
+
+sam = Person("Sam")
+
+display_info(tom)
+display_info(yarik)
+display_info(sam)
+
+ilon = Person("ilon")
+ilon.display_info()
